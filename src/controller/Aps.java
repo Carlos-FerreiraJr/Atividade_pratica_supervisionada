29c558be4f810d61d806de694e86b7647665652e
Carlos <carlosferreiratech@gmail.com>
2019-05-20 21:50:49 -0300

    aps 1.0.1

--- src/controller/Aps.java
+++ src/controller/Aps.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aps;
+package controller;
+import view.Main;
 
 /**
  *
@@ -16,6 +17,8 @@ public class Aps {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new Main().setVisible(true);
+        
     }
     
 }
